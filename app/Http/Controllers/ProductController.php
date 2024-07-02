87e0d9659f36c8735bb514695d5cd6e725486d4c
Gabriel Ifoga <gabrielifoga@yahoo.com>
2024-07-02 09:22:47 -0400

    fix bug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,23 +17,23 @@ class ProductController extends Controller
         $this->zoho_repository = $zoho_repository;
     }
 
-    public function productSyncWoocommerce(Request $request)
+    public function productSyncWoocommerce()
     {
         // $request->validate([
         //     'name' => 'required|string|max:255',
         // ]);
         $productData = [
-            'name' => $request('name'),
-            'sku' => $request('sku'),
-            'stock_quantity' => $request('stock_on_hand'),
-            'type' => $request('type', 'simple'),
-            'price' => $request('rate'), 
-            'regular_price' => $request('pricebook_rate'), 
-            'sale_price' => $request('rate'), 
-            'description' => $request('description'), 
-            'short_description' => $request('purchase_description'), 
-            'zoho_id' => $request("item_id"),
-            'images' => $request('image_name', [
+            'name' => request('name'),
+            'sku' => request('sku'),
+            'stock_quantity' => request('stock_on_hand'),
+            'type' => request('type', 'simple'),
+            'price' => request('rate'), 
+            'regular_price' => request('pricebook_rate'), 
+            'sale_price' => request('rate'), 
+            'description' => request('description'), 
+            'short_description' => request('purchase_description'), 
+            'zoho_id' => request("item_id"),
+            'images' => request('image_name', [
                 [
                     'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
                 ]
